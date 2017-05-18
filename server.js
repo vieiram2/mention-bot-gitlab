507ce416e6e1c70ad9769e62eb9e6e74f7c9a25b
zohra <fznasri@ayaline.com>
2017-05-18 14:25:12 +0200

    Debug

--- server.js
+++ server.js
@@ -106,12 +106,6 @@ app.post('/', function(req, res) {
 
                         request.post({
                             url : process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/users',
-                            body: JSON.stringify({
-                                note : messageGenerator(
-                                    reviewers,
-                                    buildMentionSentence,
-                                    defaultMessageGenerator)
-                            }),
                             headers : {
                                 'PRIVATE-TOKEN' : process.env.GITLAB_TOKEN
                             }
