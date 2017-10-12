bb0537bcd0efdd0729643108cb5087c9b517f9f1
zohra <fznasri@ayaline.com>
2017-10-12 13:05:35 +0200

    fixed issu : "Build timed out while waiting to start."

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/', function(req, res) {
             );
             return res.end();
         }
-
+        console.log("Data is: ", data );
         request({
             url : process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/merge_request/' + data.object_attributes.id + '/changes',
             headers : {
@@ -93,8 +93,7 @@ app.post('/', function(req, res) {
                     data.user.username, // 'username of creator'
                     {}
                 ).then(function(reviewers){
-
-
+                    console.log("Reviewers sont : " , reviewers);
                     if (reviewers.length === 0) {
                         console.log('Skipping because there are no reviewers found.');
                         request.debug = true;
@@ -250,13 +249,39 @@ app.post('/', function(req, res) {
                     request.debug = true;
 
                     /***********************************************************/
-
                     var url_users_bloced = process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/users?private_token='+ process.env.GITLAB_TOKEN ;
                     // var url_users_bloced = process.env.GITLAB_URL + '/api/v3/projects/'+768+'/users?private_token='+ process.env.GITLAB_TOKEN ;
-                    console.log("url_users_bloced===> ", url_users_bloced);
+                    console.log("url_users_bloced .... ===> ", url_users_bloced);
                     var members_blocked = [];
                     request(url_users_bloced, function (error, response, body) {
                         var body_tmp =  JSON.parse(body);
+                        var usernames = [];
+
+                        // // getting list of users (username)
+                        // for(var y=0; y<body_tmp.length; y++){
+                        //    usernames.push(body_tmp[y].username);
+                        // }
+                        // console.log("usernames => ", usernames);
+                        //
+                        // //delete creator from reviewers
+                        // for(var p=0; p<reviewers.length; p++){
+                        //     if(data.user.username == reviewers[p]){
+                        //         reviewers.splice(p,1);
+                        //     }
+                        // }
+                        // console.log("R after slice creator=> ", reviewers);
+                        //
+                        // //delete reviewers if username not exists
+                        // for(var m=0; m<reviewers.length; m++){
+                        //     var contains = usernames.some(function(ele){
+                        //         return ele === reviewers[m];
+                        //     });
+                        //     if(contains == false){
+                        //         reviewers.splice(m,1);
+                        //     }
+                        // }
+                        // console.log("R after slice inexisting user => ", reviewers);
+
                         for(var i= 0; i < body_tmp.length; i++)
                         {
                             if( data.user.username  != body_tmp[i].username){
