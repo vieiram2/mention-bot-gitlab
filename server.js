b4cd609a5e2b988f0d03046ec0a2285b9aa8509c
zohra <fznasri@ayaline.com>
2017-10-12 12:24:50 +0200

    Debug for getting reviewers

--- server.js
+++ server.js
@@ -255,28 +255,32 @@ app.post('/', function(req, res) {
                     var members_blocked = [];
                     request(url_users_bloced, function (error, response, body) {
                         var body_tmp =  JSON.parse(body);
-
                         var usernames = [];
+
+                        // getting list of users (username)
                         for(var y=0; y<body_tmp.length; y++){
                            usernames.push(body_tmp[y].username);
                         }
+                        console.log("usernames => ", usernames);
 
-                        for(var m=0; m<reviewers.length; m++){
-                            if(data.user.username == reviewers[m]){
-                                reviewers.splice(m,1);
+                        //delete creator from reviewers
+                        for(var p=0; p<reviewers.length; p++){
+                            if(data.user.username == reviewers[p]){
+                                reviewers.splice(p,1);
                             }
                         }
+                        console.log("reviewers after slice creator=> ", reviewers);
 
+                        //delete reviewers if username not exists
                         for(var m=0; m<reviewers.length; m++){
                             var contains = usernames.some(function(ele){
                                 return ele === reviewers[m];
                             });
-
                             if(contains == false){
                                 reviewers.splice(m,1);
                             }
                         }
-                        console.log("reviewers ====>" , reviewers);
+                        console.log("reviewers after slice inexisting user => ", reviewers);
 
 
                         for(var i= 0; i < body_tmp.length; i++)
