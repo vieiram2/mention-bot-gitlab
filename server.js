b1632372ac9860b17ce0712f5cb6908ec6966f61
zohra <fznasri@ayaline.com>
2017-10-13 11:28:18 +0200

    ignoré les utilisateur inexistant

--- server.js
+++ server.js
@@ -93,169 +93,10 @@ app.post('/', function(req, res) {
                     data.user.username, // 'username of creator'
                     {}
                 ).then(function(reviewers){
-                    console.log("Reviewers sont : " , reviewers);
-                    if (reviewers.length === 0) {
-                        console.log('Skipping because there are no reviewers found.');
-                        request.debug = true;
-                        var url = process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/users?private_token='+ process.env.GITLAB_TOKEN ;
-
-                        request(url, function (error, response, body) {
-                            var body_tmp =  JSON.parse(body);
-                            var members = [];
-                            for(var i= 0; i < body_tmp.length; i++)
-                            {
-                                if( data.user.username  != body_tmp[i].username){
-                                    members.push(body_tmp[i].username);
-                                }
-                            }
-
-                            var url_users_bloced = process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/users?private_token='+ process.env.GITLAB_TOKEN  ;
-                            // var url_users_bloced = process.env.GITLAB_URL + '/api/v3/projects/'+768+'/users?private_token='+ process.env.GITLAB_TOKEN ;
-                            var members_blocked = [];
-                            request(url_users_bloced, function (error, response, body) {
-                                var body_tmp =  JSON.parse(body);
-                                for(var i= 0; i < body_tmp.length; i++)
-                                {
-                                    if( data.user.username  != body_tmp[i].username){
-                                        if(body_tmp[i].state == "blocked" ){
-                                            members_blocked.push(body_tmp[i].username);
-                                        }
-                                    }
-                                }
-                                var members_tmp =[];
-
-                                for(var i= 0; i < members.length; i++)
-                                {
-                                    for(var j=0 ; j<members_blocked.length; j++ ){
-                                        if(members_blocked[j] !== members[i]){
-                                            members_tmp.push(members[i]);
-                                        }
-                                    }
-                                }
-                                if(members_tmp.length>0){
-                                    members = members_tmp ;
-                                }
-                                if(members.length > 2){
-                                    var rand1 = members[Math.floor(Math.random() * members.length)] ,
-                                        rand2 = members[Math.floor(Math.random() * members.length)];
-                                    members = [];
-                                    if(rand1 != rand2){
-                                        members.push(rand1);
-                                        members.push(rand2);
-                                    }else{
-                                        members.push(rand1);
-                                        rand2 = members[Math.floor(Math.random() * members.length)];
-                                        members.push(rand2);
-                                    }
-                                }else{
-                                    if(members.length == 0){
-                                        var members_g = [];
-                                        var url_groups = process.env.GITLAB_URL + '/api/v3/groups?private_token='+ process.env.GITLAB_TOKEN ,
-                                            list_groupsID = [];
-
-                                        request(url_groups, function (error, response, groups) {
-                                            var groups_tmp =  JSON.parse(groups);
-                                            for(var i= 0; i < groups_tmp.length; i++)
-                                            {
-                                                if(groups_tmp[i].visibility_level > 0){
-                                                    list_groupsID.push(groups_tmp[i].id);
-                                                }
-                                            }
-                                            if(list_groupsID.length>0){
-                                                var IdGourpsAlt = list_groupsID[Math.floor(Math.random() * list_groupsID.length)] ,
-                                                    Members_groupURL = process.env.GITLAB_URL + '/api/v3/groups/' + IdGourpsAlt + '/members?private_token='+ process.env.GITLAB_TOKEN ;
-                                                request(Members_groupURL, function (error, response, members) {
-
-                                                    var members_tmp =  JSON.parse(members),
-                                                        Members_group =[];
-                                                    if(members_tmp.length > 0){
-                                                        for(var i= 0; i < members_tmp.length; i++)
-                                                        {
-                                                            if( data.user.username  != members_tmp[i].username){
-                                                                if(members_tmp[i].state != "blocked" ){
-                                                                    Members_group.push(members_tmp[i].username);
-                                                                }
-                                                            }
-                                                        }
-
-                                                        if(Members_group.length>0){
-                                                            members_g = Members_group ;
-                                                        }
-
-                                                        if(members_g.length > 2){
-                                                            var rand1 = members_g[Math.floor(Math.random() * members_g.length)] ,
-                                                                rand2 = members_g[Math.floor(Math.random() * members_g.length)];
-                                                            members_g = [];
-                                                            if(rand1 != rand2){
-                                                                members_g.push(rand1);
-                                                                members_g.push(rand2);
-                                                            }else{
-                                                                members_g.push(rand1);
-                                                                rand2 = members_g[Math.floor(Math.random() * members_g.length)];
-                                                                members_g.push(rand2);
-                                                            }
-                                                        }
-                                                    }
-
-                                                    request.post({
-                                                        url : process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/merge_requests/' + data.object_attributes.id + '/comments',
-                                                        body: JSON.stringify({
-                                                            note : messageGenerator(
-                                                                members_g,
-                                                                data.user.username,
-                                                                buildMentionSentence,
-                                                                defaultMessageGenerator)
-                                                        }),
-                                                        headers : {
-                                                            'PRIVATE-TOKEN' : process.env.GITLAB_TOKEN,
-                                                            'Content-Type' : 'application/json'
-                                                        }
-                                                    },function(commentError, commentResponse, commentBody){
-                                                        if (commentError || commentResponse.statusCode != 200) {
-                                                            console.log('Error commenting on merge request: ' + commentBody);
-                                                        }
-                                                    });
-
-                                                });
-                                            }
-                                        });
-                                    }
-                                    return;
-                                }
-
-                                request.post({
-                                    url : process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/merge_requests/' + data.object_attributes.id + '/comments',
-                                    body: JSON.stringify({
-                                        note : messageGenerator(
-                                            members,
-                                            data.user.username,
-                                            buildMentionSentence,
-                                            defaultMessageGenerator)
-                                    }),
-                                    headers : {
-                                        'PRIVATE-TOKEN' : process.env.GITLAB_TOKEN,
-                                        'Content-Type' : 'application/json'
-                                    }
-                                },function(commentError, commentResponse, commentBody){
-                                    if (commentError || commentResponse.statusCode != 200) {
-                                        console.log('Error commenting on merge request: ' + commentBody);
-                                    }
-                                });
-                            });
-                            /***********************************************************/
-                        });
-                        return ;
-                    }
-                    request.debug = true;
-
                     /***********************************************************/
-                    var url_users_bloced = process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/users?private_token='+ process.env.GITLAB_TOKEN ;
-                    // var url_users_bloced = process.env.GITLAB_URL + '/api/v3/projects/'+768+'/users?private_token='+ process.env.GITLAB_TOKEN ;
-                    console.log("url_users_bloced .... ===> ", url_users_bloced);
-                    var members_blocked = [];
-                    request(url_users_bloced, function (error, response, body) {
+                    var url_users = process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/users?private_token='+ process.env.GITLAB_TOKEN ;
+                    request(url_users, function (error, response, body) {
                         var body_tmp =  JSON.parse(body);
-
                         var usernames =[];
                         var usernames_tmp =[];
                         // Getting list of users in this project (usernames)
@@ -264,7 +105,6 @@ app.post('/', function(req, res) {
                                 usernames_tmp.push((body_tmp[y].username));
                             }
                         }
-                        console.log("usernames ====>" , usernames_tmp);
 
                         // Delete creator from list of reviewers
                         for(var m=0; m<reviewers.length; m++){
@@ -272,8 +112,7 @@ app.post('/', function(req, res) {
                                 reviewers.splice(m,1);
                             }
                         }
-                        console.log("reviewers ====> sl1 " , reviewers );
-                        console.log("--------------------------------------" );
+                      //getting just existing users
                         var exist;
                         for(var q=0; q<usernames_tmp.length; q++)
                         {
@@ -290,39 +129,49 @@ app.post('/', function(req, res) {
                                 usernames.push(usernames_tmp[q]);
                             }
                         }
-                        console.log("usernames..........." , usernames);
-                        reviewers = usernames ;
 
+                        reviewers = usernames ;
+                        // getting just 2 users from the list of reviewers
                         if(reviewers.length > 2){
-                            var rand1 = reviewers[Math.floor(Math.random() * reviewers.length)] ,
-                                rand2 = reviewers[Math.floor(Math.random() * reviewers.length)];
-                            if(rand1 != rand2){
-                                reviewers = [];
-                                reviewers.push(rand1);
-                                reviewers.push(rand2);
-                            }else{
-                                rand2 = reviewers[Math.floor(Math.random() * reviewers.length)];
-                                reviewers = [];
-                                reviewers.push(rand1);
-                                reviewers.push(rand2);
+                            var al1 = Math.floor(Math.random() * reviewers.length);
+                            var al2 = reviewers.length-1;
+                            if(al1 == al2 && al1 !=0){
+                                al2=0;
                             }
-                        }else{
-                            if(reviewers.length == 0){
-                                console.log("usernames ====>" , usernames_tmp);
-                                var rand1 = usernames_tmp[Math.floor(Math.random() * usernames_tmp.length)] ,
-                                    rand2 = usernames_tmp[Math.floor(Math.random() * usernames_tmp.length)];
-                                if(rand1 != rand2){
-                                    reviewers = [];
-                                    reviewers.push(rand1);
-                                    reviewers.push(rand2);
-                                }else{
-                                    rand2 = usernames_tmp[Math.floor(Math.random() * usernames_tmp.length)];
-                                    reviewers = [];
-                                    reviewers.push(rand1);
-                                    reviewers.push(rand2);
+                            else{
+                                if(al1 == al2 && al1 ==0)
+                                {
+                                    al2 = reviewers.length-1;
                                 }
                             }
+                            var rand1 =  reviewers[al1], rand2 = reviewers[al2];
+                            reviewers = [];
+                            reviewers.push(rand1);
+                            reviewers.push(rand2);
+
+                        }else{
+                            // getting just 2 users from the list of members
+                            if(reviewers.length == 0){
+
+                                var al1 = Math.floor(Math.random() * usernames_tmp.length);
+                                var al2 = usernames_tmp.length-1;
+                                if(al1 == al2 && al1 !=0){
+                                    al2=0;
+                                }
+                                else{
+                                    if(al1 == al2 && al1 ==0)
+                                    {
+                                        al2 = usernames_tmp.length-1;
+                                    }
+                                }
+                                reviewers = [];
+                                reviewers.push(usernames_tmp[al1]);
+                                reviewers.push(usernames_tmp[al2]);
+
+                            }
                         }
+
+
                         request.post({
                             url : process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/merge_requests/' + data.object_attributes.id + '/comments',
                             body: JSON.stringify({
