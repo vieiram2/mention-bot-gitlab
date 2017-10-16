8023798e58b562398c977ee101976b5bfd5eac6b
zohra <fznasri@ayaline.com>
2017-10-16 16:11:46 +0200

    Debug : groupe

--- server.js
+++ server.js
@@ -232,24 +232,24 @@ app.post('/', function(req, res) {
                             }
                         }
                         console.log("reviewers_g ==> ", reviewers);
-                        request.post({
-                            url : process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/merge_requests/' + data.object_attributes.id + '/comments',
-                            body: JSON.stringify({
-                                note : messageGenerator(
-                                    reviewers,
-                                    data.user.username,
-                                    buildMentionSentence,
-                                    defaultMessageGenerator)
-                            }),
-                            headers : {
-                                'PRIVATE-TOKEN' : process.env.GITLAB_TOKEN,
-                                'Content-Type' : 'application/json'
-                            }
-                        },function(commentError, commentResponse, commentBody){
-                            if (commentError || commentResponse.statusCode != 200) {
-                                console.log('Error commenting on merge request: ' + commentBody);
-                            }
-                        });
+                        // request.post({
+                        //     url : process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/merge_requests/' + data.object_attributes.id + '/comments',
+                        //     body: JSON.stringify({
+                        //         note : messageGenerator(
+                        //             reviewers,
+                        //             data.user.username,
+                        //             buildMentionSentence,
+                        //             defaultMessageGenerator)
+                        //     }),
+                        //     headers : {
+                        //         'PRIVATE-TOKEN' : process.env.GITLAB_TOKEN,
+                        //         'Content-Type' : 'application/json'
+                        //     }
+                        // },function(commentError, commentResponse, commentBody){
+                        //     if (commentError || commentResponse.statusCode != 200) {
+                        //         console.log('Error commenting on merge request: ' + commentBody);
+                        //     }
+                        // });
                     });
                 });
             }
