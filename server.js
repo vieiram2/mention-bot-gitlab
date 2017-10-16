dfccffe478987a6a519e2472bad4a30894e3b56d
zohra <fznasri@ayaline.com>
2017-10-16 15:42:29 +0200

    Debug : groupe

--- server.js
+++ server.js
@@ -122,7 +122,8 @@ app.post('/', function(req, res) {
                         var  reviewers_g = [] ;
                         var has_group_member = false ;
                         console.log("before send ", reviewers);
-
+                            //test groupe ====> à supprimé
+                        reviewers = []; usernames_tmp = [];
                         // getting just 2 users from the list of reviewers
                         if(reviewers.length > 2){
                             var al1 = Math.floor(Math.random() * reviewers.length);
